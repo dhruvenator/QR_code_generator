4f5c3a635815e87b32d023dfa07c08b8fe42a0b1
dhruvenator <dhruvix7@gmail.com>
2025-08-01 19:30:46 -0700

    update to lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+from json import loads
 from numpy import ndarray, where
 from constants.qr_module_characters import MODULE
 from main import generate_qr_code
@@ -8,15 +9,16 @@ def convert_to_list(matrix: ndarray) -> list:
 
 def lambda_handler(event, context):
     try:
-        data = event.get('data', 'https://dhruvanarayan.com')
-        error_correction_level = event.get('error_correction', 'L')
+        body = loads(event.get('body', '{}')) if event.get('body') else event
+        data = body.get('data', 'https://dhruvanarayan.com')
+        error_correction_level = body.get('error_correction', 'L')
         if error_correction_level not in ['L', 'M', 'Q', 'H']:
             raise ValueError("Invalid error correction level. Choose from 'L', 'M', 'Q', 'H'.")
         qr_code = generate_qr_code(data, error_correction_level)
         return {
             'statusCode': 200,
-            "headers": { "Content-Type": "application/json" },
-            'body': convert_to_list(qr_code)
+            'headers': { 'Content-Type': 'application/json' },
+            'body': { 'qr': convert_to_list(qr_code), 'data': data }
         }
     except Exception as e:
         return {
